ae7b943b36b8f4293161e06e8680be72bc8f9101
John <john_william_go@dlsu.edu.ph>
2025-11-01 02:09:39 +0800

    95% done for accepting orders

--- src/main/java/AddAnOrder.java
+++ src/main/java/AddAnOrder.java
@@ -6,52 +6,99 @@ import javax.swing.*;
 
 public class AddAnOrder extends JFrame implements ActionListener {
 
-    JFrame frame;
-    JButton  button;
-    JTextField name, orderNumber, tableNumber, orderDetails;
-    JComboBox orderType, paymentMethod;
-    public AddAnOrder(){
-        frame = new JFrame("Adding an Order");
-        frame.setSize(800,600);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private final JFrame mainFrame;
+    private final JFrame addCustomer;
+    private JFrame addOrderEntries;
+    private JButton  button, buttonAddOrderItem ;
+    private JTextField name, orderNumber, tableNumber, orderDetails, quantity, cookingNotes;
+    private JComboBox orderType, paymentMethod, itemList;
+    private JLabel title,nameLabel, orderNumberLabel, tableNumberLabel,orderTypeLabel, paymentMethodLabel, orderDetailsLabel;
+
+    String[] items = {
+            "Grilled Chicken Sandwich",
+            "BBQ Pork Ribs",
+            "Beef Burger",
+            "Fish and Chips",
+            "French Fries",
+            "Onion Rings",
+            "Caesar Salad",
+            "Greek Salad",
+            "Chicken Noodle Soup",
+            "Mushroom Soup",
+            "Chocolate Cake",
+            "Vanilla Cupcake",
+            "Iced Coffee",
+            "Lemon Iced Tea",
+            "Margherita Pizza",
+            "Pepperoni Pizza",
+            "Spaghetti Bolognese",
+            "Carbonara",
+            "Pancakes",
+            "Scrambled Eggs",
+            "Chicken Wrap",
+            "Tuna Sandwich",
+            "Fried Rice",
+            "Sweet and Sour Pork",
+            "Steamed Dumplings",
+            "Steamed Buns",
+            "Garlic Bread",
+            "Croissant",
+            "Roast Chicken",
+            "Lasagna"
+    };
+
+
+    public AddAnOrder(JFrame frame){
+        this.mainFrame = frame;
+        addCustomer = new JFrame("Adding an Order - Customer Details");
+        addOrderEntries = new JFrame("Adding an Order - Customer's Order Entries");
+        customerDetailsFrame();
+
+        addCustomer.setVisible(true);
+    }
+
+    public void customerDetailsFrame(){
+        addCustomer.setSize(800,600);
+        addCustomer.setResizable(false);
+        addCustomer.setLocationRelativeTo(null);
+        addCustomer.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        addCustomer.setLayout(null);
         ImageIcon background = new ImageIcon("src/main/resources/kitchen.jpg");
         JLabel backgroundLabel = new JLabel(background);
         backgroundLabel.setBounds(0, 0, 800, 600);
 
-        JLabel title = new JLabel("Add an Order");
+        title = new JLabel("Add an Order");
         title.setFont(new Font("Arial", Font.BOLD, 40));
         title.setForeground(Color.WHITE);
         title.setBounds(50, 50, 800, 50);
 
 
-        JLabel nameLabel = new JLabel("Customer Name:");
+        nameLabel = new JLabel("Customer Name:");
         nameLabel.setForeground(Color.WHITE);
         nameLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         nameLabel.setBounds(50, 100, 800, 50);
 
-        JLabel orderNumberLabel = new JLabel("Order Number:");
+        orderNumberLabel = new JLabel("Order Number:");
         orderNumberLabel.setForeground(Color.WHITE);
         orderNumberLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         orderNumberLabel.setBounds(50, 150, 800, 50);
 
-        JLabel tableNumberLabel = new JLabel("Table Number:");
+        tableNumberLabel = new JLabel("Table Number:");
         tableNumberLabel.setForeground(Color.WHITE);
         tableNumberLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         tableNumberLabel.setBounds(50, 200, 800, 50);
 
-        JLabel orderTypeLabel = new JLabel("Order Type:");
+        orderTypeLabel = new JLabel("Order Type:");
         orderTypeLabel.setForeground(Color.WHITE);
         orderTypeLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         orderTypeLabel.setBounds(50, 250, 800, 50);
 
-        JLabel paymentMethodLabel = new JLabel("Payment Method:");
+        paymentMethodLabel = new JLabel("Payment Method:");
         paymentMethodLabel.setForeground(Color.WHITE);
         paymentMethodLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         paymentMethodLabel.setBounds(50, 300, 800, 50);
 
-        JLabel orderDetailsLabel = new JLabel("Order Details:");
+        orderDetailsLabel = new JLabel("Order Details:");
         orderDetailsLabel.setForeground(Color.WHITE);
         orderDetailsLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         orderDetailsLabel.setBounds(50, 350, 800, 50);
@@ -83,32 +130,84 @@ public class AddAnOrder extends JFrame implements ActionListener {
         button.setBounds(200, 410, 200, 30);
         button.addActionListener(this);
 
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.add(title);
-
-        frame.add(nameLabel);
-        frame.add(orderNumberLabel);
-        frame.add(tableNumberLabel);
-        frame.add(orderTypeLabel);
-        frame.add(paymentMethodLabel);
-        frame.add(orderDetailsLabel);
-
-        // Text Fields
-        frame.add(name);
-        frame.add(orderNumber);
-        frame.add(tableNumber);
-        frame.add(orderType);
-        frame.add(paymentMethod);
-        frame.add(orderDetails);
-
-        // Button
-        frame.add(button);
-
-        frame.add(backgroundLabel);
-
-
+        addCustomer.add(title);
+        addCustomer.add(nameLabel);
+        addCustomer.add(orderNumberLabel);
+        addCustomer.add(tableNumberLabel);
+        addCustomer.add(orderTypeLabel);
+        addCustomer.add(paymentMethodLabel);
+        addCustomer.add(orderDetailsLabel);
+        addCustomer.add(name);
+        addCustomer.add(orderNumber);
+        addCustomer.add(tableNumber);
+        addCustomer.add(orderType);
+        addCustomer.add(paymentMethod);
+        addCustomer.add(orderDetails);
+        addCustomer.add(button);
+        addCustomer.add(backgroundLabel);
+        addCustomer.setVisible(true);
     }
+
+    public void customerOrderEntries(){
+        addOrderEntries.setSize(800,600);
+        addOrderEntries.setResizable(false);
+        addOrderEntries.setLocationRelativeTo(null);
+        addOrderEntries.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        addOrderEntries.setLayout(null);
+        ImageIcon background2 = new ImageIcon("src/main/resources/kitchen.jpg");
+        JLabel backgroundLabel2 = new JLabel(background2);
+        backgroundLabel2.setBounds(0, 0, 800, 600);
+
+        JLabel title = new JLabel("Insert Order Details");
+        title.setFont(new Font("Arial", Font.BOLD, 40));
+        title.setForeground(Color.WHITE);
+        title.setBounds(50, 50, 800, 50);
+
+        JLabel customerNameDisplay = new JLabel("For Customer: " + name.getText());
+        customerNameDisplay.setFont(new Font("Arial", Font.PLAIN, 16));
+        customerNameDisplay.setForeground(Color.WHITE);
+        customerNameDisplay.setBounds(50, 80, 800, 50);
+
+        JLabel selectItemLabel = new JLabel("Select Item:");
+        selectItemLabel.setForeground(Color.WHITE);
+        selectItemLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        selectItemLabel.setBounds(50, 140, 800, 50);
+        itemList = new JComboBox(items);
+        itemList.setBounds(200, 150, 200, 30);
+        itemList.addActionListener(this);
+
+
+        JLabel quantityLabel = new JLabel("Quantity: ");
+        quantityLabel.setForeground(Color.WHITE);
+        quantityLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        quantityLabel.setBounds(50, 190, 800, 50);
+        quantity = new JTextField();
+        quantity.setBounds(200, 200, 200, 30);
+
+        JLabel cookingNotesLabel = new JLabel("Cooking Notes: ");
+        cookingNotesLabel.setForeground(Color.WHITE);
+        cookingNotesLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        cookingNotesLabel.setBounds(50, 240, 800, 50);
+        cookingNotes = new JTextField();
+        cookingNotes.setBounds(200, 250, 200, 30);
+
+        buttonAddOrderItem = new JButton("Add Item To Order");
+        buttonAddOrderItem.setBounds(200, 300, 200, 30);
+        buttonAddOrderItem.addActionListener(this);
+
+        addOrderEntries.add(title);
+        addOrderEntries.add(buttonAddOrderItem);
+        addOrderEntries.add(customerNameDisplay);
+        addOrderEntries.add(selectItemLabel);
+        addOrderEntries.add(quantityLabel);
+        addOrderEntries.add(cookingNotesLabel);
+        addOrderEntries.add(itemList);
+        addOrderEntries.add(quantity);
+        addOrderEntries.add(cookingNotes);
+        addOrderEntries.add(backgroundLabel2);
+        addOrderEntries.setVisible(true);
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -131,22 +230,91 @@ public class AddAnOrder extends JFrame implements ActionListener {
                         "VALUES (?,?,?,?,?,?)";
 
                 PreparedStatement statement = connection.prepareStatement(sql);
-                statement.setString(1, customerNameField); // For CustomerName
-                statement.setInt(2, Integer.parseInt(String.valueOf(orderNumberField)));     // For OrderNumber (parsed int)
-                statement.setInt(3, Integer.parseInt(String.valueOf(tableNumberField))); // For TableNumber
-                statement.setString(4, orderTypeField);   // For OrderType
-                statement.setString(5, paymentMethodField); // For PaymentMethod
+                statement.setString(1, customerNameField);
+                statement.setInt(2, orderNumberField);
+                statement.setInt(3, tableNumberField);
+                statement.setString(4, orderTypeField);
+                statement.setString(5, paymentMethodField);
                 statement.setString(6, orderDetailsField);
 
                 statement.executeUpdate();
 
+
                 JOptionPane.showMessageDialog(null, "Customer Added Successfully!");
+
+                addCustomer.setVisible(false);
+                customerOrderEntries();
+                addOrderEntries.setVisible(true);
 
                 statement.close();
                 connection.close();
 
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
+            }
+        }
+        if  (e.getSource() == buttonAddOrderItem) {
+            try {
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:mysql://127.0.0.1:3306/ccinfomdb",
+                        "root",
+                        "12345678"
+                );
+
+                String sql = "INSERT INTO orderentries (KotID, ItemID, Quantity, CookingNotes) " +
+                        "VALUES (?,?,?,?)";
+
+                // since we need the kot id value by using the customer name given to us...
+                String kotid_statement = "SELECT KotID FROM kitchenorderticket WHERE CustomerName = ? ORDER BY KotID DESC LIMIT 1";
+                String itemID_statement = "SELECT itemID FROM menuitems WHERE ItemName = ?";
+                PreparedStatement s1 = connection.prepareStatement(kotid_statement);
+                PreparedStatement s2 = connection.prepareStatement(itemID_statement);
+
+                s1.setString(1, name.getText());
+                ResultSet kotidOfCustomer = s1.executeQuery();
+                int kotID = -1;
+                if (kotidOfCustomer.next()) {
+                    kotID = kotidOfCustomer.getInt("KotID");
+                } else {
+                    JOptionPane.showMessageDialog(null, "KotID not found for customer!");
+                    return; // stop execution
+                }
+//                ResultSet kotidOfCustomer = s1.executeQuery();
+//                kotidOfCustomer.next();
+//                int kotID = kotidOfCustomer.getInt("KotID");
+
+                kotidOfCustomer.close();
+
+                String selectedItem = (String) itemList.getSelectedItem(); // get the item from combo box
+                s2.setString(1, selectedItem);
+
+                ResultSet itemIDofMenuItem = s2.executeQuery();
+                itemIDofMenuItem.next();
+                String itemID = itemIDofMenuItem.getString("ItemID");
+
+
+                s1.close();
+                s2.close();
+
+                // we need the item id value
+
+
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setInt(1, kotID);
+                statement.setString(2, itemID);
+                statement.setString(3, quantity.getText());
+                statement.setString(4, cookingNotes.getText());
+
+                statement.executeUpdate();
+
+
+                JOptionPane.showMessageDialog(null, "Order Added Successfully!");
+                statement.close();
+                connection.close();
+
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+
             }
         }
     }
@@ -180,3 +348,4 @@ public class AddAnOrder extends JFrame implements ActionListener {
     //
     //Now you are gonna integrate it with a GUI using swing.
 // Test update
+// a shotgun next to me would be nice uwu
